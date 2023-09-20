35eb7b3b4e9b561658b55c157cfed42a10183236
FabrizioMoreno <95447605+FabrizioMoreno@users.noreply.github.com>
2023-09-20 10:13:21 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,17 @@
 <body>
     <h1>Lista de Usuarios</h1>
     <?php include 'archivoPHP.php'; ?>
+
+    <h1>Agregar usuario</h1>
+
+    <form action="archivoSPHP.php" method="post">
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
+        <input type="submit" value="Insertar Usuario">
+    </form>
+    
 </body>
 </html>
